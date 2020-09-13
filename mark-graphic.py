9d33aa2b0f1c558377eea819a1e892cc352e1787
gruinelli <caco3@ruinelli.ch>
2020-09-13 23:36:44 +0200

    prepared to use meta data

--- mark-graphic.py
+++ mark-graphic.py
@@ -1,12 +1,16 @@
 import logging
 import argparse
+import time
 import datetime
+import pytz
+import json
 from PIL import Image, ImageDraw
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Script to take a forecast graph image file (png) and add a mark to the current time')
     parser.add_argument('-v', action='store_true', help='Verbose output')
     parser.add_argument('-i', action='store', type=argparse.FileType('r'), required=True,  help='Input file name of the graph image (png)')
+    parser.add_argument('-m', action='store', type=argparse.FileType('r'), help='Meta file to be used (json)')
     parser.add_argument('-o', action='store', type=argparse.FileType('w'), required=True,  help='Output file name of the graph image (png)')
     parser.add_argument('-x', action='store', type=int, required=True, help='start-x pixel of first day')
     parser.add_argument('-y', action='store', type=int, required=True, help='start-x pixel of first day')
@@ -45,6 +49,21 @@ if __name__ == '__main__':
         d.line([(x + w, y), (x + w, y + h)], fill='red', width=1) # right side
         d.line([(x, y), (x + w, y)], fill='red', width=1) # top
         d.line([(x, y + h), (x + w, y + h)], fill='red', width=1) # bottom
+
+
+    if args.m:
+        # Get offset from local time to UTC, see also https://stackoverflow.com/questions/3168096/getting-computers-utc-offset-in-python
+        ts = time.time()
+        utcOffset = (datetime.datetime.fromtimestamp(ts) -
+                    datetime.datetime.utcfromtimestamp(ts)).total_seconds()
+        utcOffset = int(utcOffset / 3600) # in hours
+        logging.debug("UTC offset: %dh" % utcOffset)
+
+        with open(args.m.name) as metaFile:
+            metaData = json.load(metaFile)
+        logging.debug("%d seconds since last model generation" % (datetime.datetime.utcnow().timestamp() + utcOffset * 3600 - metaData['modelTimestamp']))
+
+        # TODO check if mode got generated yesterday. if so, the current time must be shown on the 2nd day shown instead of the first! 
 
 
     if args.fake_time:
